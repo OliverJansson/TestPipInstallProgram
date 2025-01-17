5558c02c8036ca08441ea36dd9cbea18ae3d69ad
OliverJansson <billy27000@gmail.com>
2025-01-17 16:09:25 +0100

    Thired Commit, added hinting for varibles in functions

--- TestPipInstallProgram/TestPipInstallProgram/simple_calc.py
+++ TestPipInstallProgram/TestPipInstallProgram/simple_calc.py
@@ -1,21 +1,21 @@
 # Simple Calculater, attraction, subtraction, multiplication and division
 
-def attraction(a,b):
+def attraction(a:float,b:float):
     #Adds a and b together.
     c = a+b
     return(c)
 
-def subtraction(a,b):
+def subtraction(a:float,b:float):
     #subtracts b from a.
     c = a-b
     return(c)
 
-def multiplication(a,b):
+def multiplication(a:float,b:float):
     #Multiply a and b.
     c = a*b
     return(c)
 
-def division(a,b):
+def division(a:float,b:float):
     #divide a by b.
     try:    # Error handling if divition by 0
         c = a/b
